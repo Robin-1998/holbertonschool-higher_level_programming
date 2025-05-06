43156a6eae0e48a2b353e23319dc582a1193197d
Robin-1998 <10616@holbertonstudents.com>
2025-05-06 13:33:10 +0200

    ajout du fichier pour la tache 4

---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for alphabet in range(99):
+    print("{:d} = 0x{:x}".format(alphabet, alphabet))
+
+# {:d} est un spécificateur de format utilisé pour formater un entier (décimal)
+# {:x} est un spécificateur de format qui convertit un entier en héxadécimal
+# dans une chaîne de caractère
