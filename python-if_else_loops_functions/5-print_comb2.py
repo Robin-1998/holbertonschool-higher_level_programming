7fb6c4b43d278cab433979131b3efaa59afbe74d
Robin-1998 <10616@holbertonstudents.com>
2025-05-06 14:13:15 +0200

    correctif 1.1 autopep8 for task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for alphabet in range(100):
     if alphabet < 99:
-        print("{:02d}, ".format(alphabet),end='')
-#prend tout en compte sauf les deux dernier caractères dernier caractère vu
+        print("{:02d}, ".format(alphabet), end='')
+# prend tout en compte sauf les deux dernier caractères dernier caractère vu
     else:
         print("{:02d}".format(alphabet))
-#l'avant dernier caractère affiche le dernier mot mais sans la virgule
+# l'avant dernier caractère affiche le dernier mot mais sans la virgule
