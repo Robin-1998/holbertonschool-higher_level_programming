f13f7e0dbb08d6324e028e6030eb15d48dfd47c2
Robin-1998 <10616@holbertonstudents.com>
2025-05-07 11:03:33 +0200

    ajout des fichiers pour la tache 0 + README

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from add_0 import add
+if __name__ == "__main__":
+    a = 1
+    b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
+# sert à indiquer que le bloc de code qui suit doit être exécuté uniquement
+# si le fichier est exécuté directement, et pas lorsqu'il est importé
+# comme module dans un autre fichier.
