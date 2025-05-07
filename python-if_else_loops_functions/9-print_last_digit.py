2c5abcc2487e13a220866b0e426329313cb0462b
Robin-1998 <10616@holbertonstudents.com>
2025-05-07 09:38:28 +0200

    correctif tache 9 avec ajout de la valeur absolu

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return (number % 10)
+    print(abs(number) % 10, end='')
+    return (abs(number) % 10)
+# abs(number) : prend la valeur absolue du nombre.Cela transforme un
+# nombre négatif en positif. Par exemple, abs(-278) devient 278.
+# %10, permet d'obtenir le dernier chiffre
