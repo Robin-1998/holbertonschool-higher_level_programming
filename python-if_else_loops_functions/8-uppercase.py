553732face1691d3b7a2d259fc03e908bd789075
Robin-1998 <10616@holbertonstudents.com>
2025-05-09 11:31:59 +0200

    ajout du fichier pour la tache 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 def uppercase(str):
+    majuscule = ""
+    for i in range(len(str)):
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+            majuscule += chr(ord(str[i]) - 32)
+            continue
+        majuscule += str[i]
+
+    print('{0}'.format(majuscule))
