2fcf95382001c4e5dcb58dda64f6b4eded844073
Robin-1998 <10616@holbertonstudents.com>
2025-05-12 10:47:44 +0200

    correctif tache 1 avec un return

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return("None")
-    if idx > my_list[idx]:
-        return("None")
+    if idx < 0 or idx > my_list[idx]:
+        return None
+    return (my_list[idx])
