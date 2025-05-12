54641c2b276c341a177d630e6721442e33ace7ba
Robin-1998 <10616@holbertonstudents.com>
2025-05-12 10:16:12 +0200

    changement syntaxe tache 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print("{}".format(integer))
+    for integer in range (len(my_list)):
+        print("{}".format(my_list[integer]))
 
 # deuxième méthode de boucle pour la tâche 0
 # for integer in range(len(my_list)):
