0b0a97133a374bec4a4dd5799e51a98173b10567
Robin-1998 <10616@holbertonstudents.com>
2025-05-12 10:22:56 +0200

    ajout de :d dans le spécifieur

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for integer in range (len(my_list)):
-        print("{}".format(my_list[integer]))
+    for integer in my_list:
+        print("{:d}".format(integer))
+
+# deuxième méthode de boucle pour la tâche 0
+# for integer in range(len(my_list)):
