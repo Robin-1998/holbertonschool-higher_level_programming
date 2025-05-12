1745e84bc8fa513315e51cc0667c50cb24174bb0
Robin-1998 <10616@holbertonstudents.com>
2025-05-12 10:51:16 +0200

    modif tache 0

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > my_list[idx]:
+    if idx < 0 or idx > len(my_list):
         return None
     return (my_list[idx])
+
+# Si le return (my_list[idx]) était absent, l’appel element_at(liste, 2)
+# n’imprimerait rien (ou None), car la fonction ne renverrait aucune valeur.
+
