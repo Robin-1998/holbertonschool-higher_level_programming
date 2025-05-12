03245c9c89027595459df0801515d70dfa1167a5
Robin-1998 <10616@holbertonstudents.com>
2025-05-12 11:18:02 +0200

    modif tache 2, changement de valeur entre 4 et 9

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,4 +4,7 @@ def replace_in_list(my_list, idx, element):
         return my_list
     if idx >= len(my_list):
         return my_list
+    my_list[idx] = element
     return my_list
+
+# on échange la valeur de notre élément avec la valeur 4 du tableau my_listt
