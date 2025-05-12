b15a8c43a40eaf9d0e9d579485c1e65c56a43cca
Robin-1998 <10616@holbertonstudents.com>
2025-05-12 10:01:49 +0200

    ajout du README + ajout du fichier pour la tache 0

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for integer in my_list:
+        print("{}".format(integer))
+
+# deuxième méthode de boucle
+# for integer in range(len(my_list)):
