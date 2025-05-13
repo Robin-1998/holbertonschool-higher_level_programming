09172ddd7f63ba433a910caa2013f6702d1e0436
Robin-1998 <10616@holbertonstudents.com>
2025-05-13 16:20:53 +0200

    ajout du fichier pour la tache 6

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for a in sorted(a_dictionary):
+        print(f"{a}: {a_dictionary[a]}")
+
+# la fonction sorted permet de ranger dans l'ordre
