a235ae71e1d5804a05bfa849f088e5d72b8fe295
Robin-1998 <10616@holbertonstudents.com>
2025-05-13 13:44:19 +0200

    correctif variable + indentation

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,11 +4,13 @@ def square_matrix_simple(matrix=[]):
     for i in matrix:
         stock = []
         for j in i:
-            stock.append(j * j)  # on multiplie toute nos valeur
-        grand_stock.append(stock)  # on les intègre dans notre grand tableau
-# vu que c'est un tableau en 2 dimensions
-    return stock
+            stock.append(j * j)
+        grand_stock.append(stock)
+    return grand_stock
 
+# ligne 7: on multiplie toute nos valeur
+# ligne 8 : on les intègre dans notre grand tableau vu
+# ligne 8 : que c'est un tableau en 2 dimensions
 
 # les [] permet de stocker les lignes de la grande matrice qui contient
 # les racines carrés
