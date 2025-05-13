f7707396301acf776fd12c7da70229b0f0de3a7f
Robin-1998 <10616@holbertonstudents.com>
2025-05-13 09:34:47 +0200

    correctif tache 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == "":
+    if not my_list:
         return None
     largest = my_list[0]
     for i in my_list:
         if i > largest:
             largest = i
-    return (largest)
+    return largest
 
 # on parcourt la liste et on va chercher la plus grande valeur
 # Si l'élément actuel est supérieur au plus grand on le met à jour
