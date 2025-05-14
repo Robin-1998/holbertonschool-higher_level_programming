93aa2c51ac6a30b89ae2ca9dee81e78dad1fcfaa
Robin-1998 <10616@holbertonstudents.com>
2025-05-14 11:26:36 +0200

    correctif variable

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,8 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary, i=a_dictionary.get)
+    return max(a_dictionary, key=a_dictionary.get)
 
 # la fonction max va chercher l'élément le plus grand dans notre dictionnaire
 # le .get permet de récupérer la valeur associé à notre clé du dictionnaire
-# la valeur est présenté sous le nom de la variable i
+# la clé est donc représenté par le mot reservé : "key"
