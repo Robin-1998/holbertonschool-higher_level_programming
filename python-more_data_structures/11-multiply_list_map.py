3cefdbecf8a62867203713e738ade7b3e498edb2
Robin-1998 <10616@holbertonstudents.com>
2025-05-14 12:16:47 +0200

    correctif pour avoir 3 ligne et non plusieurs pour le checker

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# fonction qui renvoie une liste avec toute les valeurs multiplié par un
-# nombre sans utiliser de loupe.
-
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
+
+# fonction qui renvoie une liste avec toute les valeurs multiplié par un
+# nombre sans utiliser de loupe.
