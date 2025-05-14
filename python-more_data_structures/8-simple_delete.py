3165e1fdb25512a8caa22b4db9c09942b2a0a5cf
Robin-1998 <10616@holbertonstudents.com>
2025-05-14 09:46:36 +0200

    ajout d'une condition pour préciser si notre argument est présent dans le dictionnaire

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,5 +2,6 @@
 # fonction qui supprime une clé du dictionnaire
 
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
