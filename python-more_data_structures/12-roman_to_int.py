810057736207d2a80c3340d9f69e72fbaa8a76ac
Robin-1998 <10616@holbertonstudents.com>
2025-05-14 14:38:33 +0200

    ajout du fichier pour la tache 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,28 @@
 def roman_to_int(roman_string):
     if roman_string == "" or roman_string is None:
         return 0
-    if roman_string == 'X':
-        return 10
-    if roman_string == 'VII':
-        return 7
-    if roman_string == 'IX':
-        return 9
-    if roman_string == 'LXXXVII':
-        return 87
-    if roman_string == 'DCCVII':
-        return 707
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    res = 0
+    i = 0
+
+    while (i < len(roman_string)):
+        s1 = roman_dict.get(roman_string[i])
+        if (i + 1 < len(roman_string)):
+            s2 = roman_dict.get(roman_string[i + 1])
+            if (s1 >= s2):
+                res = res + s1
+                i = i + 1
+            else:
+                res = res + s2 - s1
+                i = i + 2
+        else:
+            res = res + s1
+            i = i + 1
+    return res
+
+# on utilise .get pour revoyer la valeur de l'élément roman_dict avec
+# la clé spécifié
