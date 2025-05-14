53ca644ec4c826ad8935664e4798e1d15d028971
Robin-1998 <10616@holbertonstudents.com>
2025-05-14 12:17:18 +0200

    correctif tache 11 identation

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
-
-# fonction qui renvoie une liste avec toute les valeurs multiplié par un
-# nombre sans utiliser de loupe.
