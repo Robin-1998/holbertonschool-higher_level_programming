d753cef2c5ed0737df95410b8ac913e8cc24638d
Robin-1998 <10616@holbertonstudents.com>
2025-05-15 13:42:34 +0200

    ajout du fichier pour la tache 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# fonction qui imprime les x premiers éléments de la liste et
+# seulement les entiers
+def safe_print_list_integers(my_list=[], x=0):
+# on initialise notre compteur pour parcourir ta boucle
+    compteur = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            compteur += 1
+        except (ValueError, IndexError):
+            continue  # on sort de la liste après
+    print()
+    return compteur
