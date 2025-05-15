c2a3b89130f3b26f3ec32588d8d477092e728d9b
Robin-1998 <10616@holbertonstudents.com>
2025-05-15 15:35:48 +0200

    ajout du fichier pour la tache 3

---
+++ python-exceptions/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#eleves_poudlard = ["Harry Potter", "Hermione Granger", "Ron Weasley", "Drago Malfoy",
+                  #"Neville Londubat", "Luna Lovegood", "Ginny Weasley", "Cho Chang"]
+# eleves_poudlard.append('Cedric Diggory')
+# eleves_poudlard.pop(3)
+# print(eleves_poudlard[:3])
+# print (sorted(eleves_poudlard))
+    # eleves_poudlard.sort()
+    # print (eleves_poudlard)
+
+# PARTIE 2: MANIPULATION DE DICTIONNAIRES
+
+maisons = {
+    "Harry Potter": "Gryffondor",
+    "Hermione Granger": "Gryffondor",
+    "Ron Weasley": "Gryffondor",
+    "Draco Malfoy": "Serpentard",
+    "Luna Lovegood": "Serdaigle",
+    "Cho Chang": "Serdaigle",
+    "Cedric Diggory": "Poufsouffle",
+    "Neville Londubat": "Gryffondor"
+}
+# maisons['Ginny Weasley'] = 'Gryffondor'
+    # print (maisons)
+
+compteur = 0
+for i in maisons:
+    if maisons[i] == "Gryffondor":
+        maisons.counts
+    print(f"{maisons[i]}: {compteur}")
+
