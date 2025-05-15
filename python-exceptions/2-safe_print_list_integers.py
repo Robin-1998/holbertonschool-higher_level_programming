30e379fbcac6edd90610a3a110365b6c2db24a2b
Robin-1998 <10616@holbertonstudents.com>
2025-05-15 14:25:05 +0200

    correctif tache 2 autopep 8 + remplacement du continue par un pass

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,15 @@
 # fonction qui imprime les x premiers éléments de la liste et
 # seulement les entiers
 def safe_print_list_integers(my_list=[], x=0):
-# on initialise notre compteur pour parcourir ta boucle
+    # on initialise notre compteur pour parcourir ta boucle
     compteur = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             compteur += 1
-        except (ValueError, IndexError):
-            pass  # on sort de la liste après
+        except (ValueError, TypeError):
+            # on gère le cas de valeur erronée dans un type de données
+            # spécifié + apparaît lorsque deux types différents sont combinés
+            pass  # on passe à la liste suivante
     print()
     return compteur
