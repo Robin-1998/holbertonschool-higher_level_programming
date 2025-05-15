5436d2bb18356fcc6a0423701f65b0f9641f4718
Robin-1998 <10616@holbertonstudents.com>
2025-05-15 13:43:28 +0200

    ajout d'un pass au lieu d'un continu

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             compteur += 1
         except (ValueError, IndexError):
-            continue  # on sort de la liste après
+            pass  # on sort de la liste après
     print()
     return compteur
