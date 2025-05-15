18a4e18aaa9eac18365b38724e2cfa631e66f138
Robin-1998 <10616@holbertonstudents.com>
2025-05-15 16:18:05 +0200

    ajout du fichier pour la tache 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    tableau = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+# on divise nos 2 éléments en les parcourtant à l'aide de la vrble index
+        except TypeError:
+            print("wrong type")
+            result = 0 # on réinitialise result à zéro pour préciser
+# que la variable existe toujours
+        except IndexError:
+            print("out of range")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        finally:
+            tableau.append(result) # on ajoute result à la fin de notre liste
+    return tableau
