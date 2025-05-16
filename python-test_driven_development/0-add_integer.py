82f1375e888a2c1fd929db1fad9a6018cfa09bec
Robin-1998 <10616@holbertonstudents.com>
2025-05-16 11:31:48 +0200

    correctif commentaire 1.2

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,12 +3,14 @@ Projet Holberton
 ROBIN D
 Write a function that adds 2 integers.
 """
+
+
 def add_integer(a, b=98):
     """
         Return the factorial of n, an exact integer >= 0.
     """
-    if not isinstance (a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance (b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
