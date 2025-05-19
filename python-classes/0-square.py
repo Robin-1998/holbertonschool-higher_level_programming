64b21bcf21d63e267b7a17cd74926bf5b4e3197b
Robin-1998 <10616@holbertonstudents.com>
2025-05-19 10:14:13 +0200

    ajout du fichier pour la tache 0 + README

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+Classe vide qui définit un carré
+"""
+
+
+class Square:
+    """
+        on définit le carré dans la class Square
+    """
+    Square = "{}"
