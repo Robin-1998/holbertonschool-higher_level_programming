71e593dde4675fdd0b7400b3257763f8d993051a
Robin-1998 <10616@holbertonstudents.com>
2025-05-20 13:17:55 +0200

    correctif commentaire tache 2 and correctif mauvaise syntaxe

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -8,22 +8,25 @@ Classe vide qui définit un rectangle
 
 class Rectangle:
     """ Une class vide dans le rectangle """
-    pass
 
     def __init__(self, width=0, height=0):
+        """ instantation"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """ private instante attribute width propriété pour récupérer width """
         return self.__width
 
     @property
     def height(self):
+        """ private instante attribute heigh propriété pour récupérer heigh """
         return self.__height
 
     @width.setter
     def width(self, value):
+        """ private instante attribute width. pour fixer width """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -32,6 +35,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """ private instante attribute height. pour fixer height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -39,10 +43,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """ Public instance method qui retourne la surface du rectangle """
         return (self.__height * self.__width)
 
     def perimeter(self):
+        """ Public instance method qui retourne le périmètre du rectangle """
         if self.__width == 0 or self.__height == 0:
-            perimeter = 0
-        perimeter = (self.__width + self.__height) * 2
-        return perimeter
+            return 0
+        return (self.__width + self.__height) * 2
