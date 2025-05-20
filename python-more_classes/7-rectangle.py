cf7a85a612897f01bcfa8bed6c0c5947861336b1
Robin-1998 <10616@holbertonstudents.com>
2025-05-20 16:02:14 +0200

    correctif indentation

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -60,8 +60,8 @@ class Rectangle:
         """On répète la ligne de # autant de fois que la hauteur e"""
         if self.__width == 0 or self.__height == 0:
             return ("")
-        return '\n'.join(str(self.print_symbol) *
-        self.__width for _ in range(self.__height))
+        return '\n'.join(str(self.print_symbol) * 
+                         self.__width for _ in range(self.__height))
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
