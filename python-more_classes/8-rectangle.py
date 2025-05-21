81e1b88f593924925ee6d218558a0372fc290c04
Robin-1998 <10616@holbertonstudents.com>
2025-05-21 09:11:43 +0200

    correctif syntaxe

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -78,5 +78,3 @@ class Rectangle:
             TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        else:
-            return rect_2
