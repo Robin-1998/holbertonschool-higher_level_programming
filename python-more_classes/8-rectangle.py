4cc7d5f5941ada3d033269725806f9568aa9e5a2
Robin-1998 <10616@holbertonstudents.com>
2025-05-21 09:17:33 +0200

    erreur de push lors du rect_1 et 2 coorigé

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -78,5 +78,5 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() > rect_2.area():
-            return rect_1
-        return rect_2
+            return rect_2
+        return rect_1
