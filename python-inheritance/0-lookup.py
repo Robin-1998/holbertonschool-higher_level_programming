1c322743c9f76db1d4e022009bceedc696cc4f63
Robin-1998 <10616@holbertonstudents.com>
2025-05-26 10:41:21 +0200

    correctif tache 0 - commentaire

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+fonction qui renvoie la liste des attributs et méthodes disponibles d'un objet
+"""
+
+
 def lookup(obj):
+    """ retourne la liste des objets """
     return dir(obj)
 # la fonction dir affiche tout le contenu d'un objet
