a17c33555f0b8bca60af4d60eca0fccd2d54a73b
Robin-1998 <10616@holbertonstudents.com>
2025-05-26 12:53:36 +0200

    correctif autopep8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,8 +6,8 @@ fonction qui renvoie True si l'objet est exactement une instance
 de la classe spécifiée ; sinon False
 """
 
+
 def is_same_class(obj, a_class):
     """ on vérifie si obj est une instance de la class a_class"""
     return type(obj) is a_class
 # type(obj) permet de renvoyer la classe exacte de l'objet : obj
-
