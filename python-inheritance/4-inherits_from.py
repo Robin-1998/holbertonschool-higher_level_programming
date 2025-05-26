dbd27d74c8739dc659ac72dcb8e496f0e8b37c2e
Robin-1998 <10616@holbertonstudents.com>
2025-05-26 16:38:55 +0200

    correctif autopep8 1.1

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,14 +3,12 @@
 Projet Holberton
 ROBIN D
 fonction qui renvoie True si l'objet est une instance d'une class qui a 
-hérité(directement ou indirectement) 
-de la classe spécifiée ; sinon False
+hérité(directement ou indirectement) de la classe spécifiée ; sinon False
 """
 
 
 def inherits_from(obj, a_class):
     """ on retourne un test ; si un objet est une instance d’une 
     sous-classe d’une classe donnée, tout en excluant les instances 
-    directes de cette classe.
-    """
+    directes de cette classe."""
     return isinstance(obj, a_class) and type(obj) is not a_class
