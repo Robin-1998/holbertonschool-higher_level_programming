c0c66a9ff14d7e1cfa22250d4b90d0254214fd1d
Robin-1998 <10616@holbertonstudents.com>
2025-05-26 11:28:59 +0200

    documentation manquante pour la class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,7 +7,10 @@ Class My_list qui hérite de list
 
 
 class MyList(list):
+    """
+    MyList est en héritage par rapport à list
+    """
+
     def print_sorted(self):
         """ imprime la liste dans l'ordre croissant"""
         print(sorted(self))
-# MyList est en héritage par rapport à list
