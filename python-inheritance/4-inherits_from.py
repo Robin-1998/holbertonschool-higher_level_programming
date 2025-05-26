f772d4f9ae6203f47b10c7e05c3f588b3276d06b
Robin-1998 <10616@holbertonstudents.com>
2025-05-26 16:34:58 +0200

    ajout d'un fichier pour la tache 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+fonction qui renvoie True si l'objet est une instance d'une class qui a hérité
+(directement ou indirectement) de la classe spécifiée ; sinon False
+"""
+
+
+def inherits_from(obj, a_class):
+    """ on retourne un test ; si un  objet est une instance d’une 
+    sous-classe d’une classe donnée, tout en excluant les instances 
+    directes de cette classe.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
