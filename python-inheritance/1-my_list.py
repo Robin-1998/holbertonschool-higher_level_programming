382488194d389f6cd56bf612d82d1d2fa6cd038d
Robin-1998 <10616@holbertonstudents.com>
2025-05-26 11:15:13 +0200

    ajout du fichier pour la tache 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+Class My_list qui hérite de list
+
+
+"""
+
+
+class List:
+    def print_sorted(self):
+        """ imprime la liste dans l'ordre croissant"""
+        print(sorted(self))
