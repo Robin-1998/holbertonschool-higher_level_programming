857266158a498eb51fe3f9540d5d22887b3ad1ab
Robin-1998 <10616@holbertonstudents.com>
2025-05-27 10:04:35 +0200

    correctif for checker

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,12 +7,14 @@ Classe BaseGeometry with test
 
 
 class BaseGeometry:
-    """Classe vide"""
+    """Classe BaseGeometry"""
 
     def area(self):
+        """ Public instance method with an exception """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """ Public instance method with a multiple exception """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
