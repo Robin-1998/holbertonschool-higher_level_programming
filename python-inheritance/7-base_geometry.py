61f2a1a6ead939bc04464f2091a5649dc4e11cd7
Robin-1998 <10616@holbertonstudents.com>
2025-05-27 09:29:44 +0200

    suppression de test + correctif autopep 8

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,14 +5,18 @@ ROBIN D
 Classe BaseGeometry with test
 """
 
+
 class BaseGeometry:
     """Classe vide"""
 
     def area(self):
+        """ public instance method with an exception"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
-        isinstance (name, str)
-        if not isinstance (value, int):
+        """ public instance method with other exception"""
+        isinstance(name, str)
+        if not isinstance(value, int):
             raise TypeError("<name> must be an integer")
         if value <= 0:
             raise ValueError("<name> must be greater than 0")
