8af23c403455341006bf0514ac308b0bf4315e1c
Robin-1998 <10616@holbertonstudents.com>
2025-05-27 10:46:40 +0200

    correctif width

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -54,4 +54,4 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
         self.integer_validator("height", height)
-        self.integer_validator("width", height)
+        self.integer_validator("width", width)
