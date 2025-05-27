c9800d1ad5871a425edcad80ac4a1c571ab3a708
Robin-1998 <10616@holbertonstudents.com>
2025-05-27 09:25:08 +0200

    ajout du fichier pour la tache 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+Classe BaseGeometry with test
+"""
+
+class BaseGeometry:
+    """Classe vide"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        isinstance (name, str)
+        if not isinstance (value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
