a58188c1d1ae2a0ef35507257349f834387969ab
Robin-1998 <10616@holbertonstudents.com>
2025-05-27 13:23:18 +0200

    ajout du fichier pour la tache 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Ce module contient la définition d'une classe de base pour des formes
+géométriques.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class Rectangle qui hérite de basegeometry"""
+
+    def __init__(self, width, height):
+        """ instantation """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+
+    def area(self):
+        print(f"[Rectangle] {self.__width}/{self.__height}")
+        return self.__height * self.__width
