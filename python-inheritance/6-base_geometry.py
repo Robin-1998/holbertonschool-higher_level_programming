ab956949d164cab27b9d08b8f0505a500bb95fd3
Robin-1998 <10616@holbertonstudents.com>
2025-05-27 08:49:37 +0200

    ajout du fichier pour la tache 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+Classe BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Classe vide"""
+    def area(self): 
+        raise Exception ("area() is not implemented")
