76b5e179eb81c983932d72479b7403fceb77a2ca
Robin-1998 <10616@holbertonstudents.com>
2025-05-28 16:13:26 +0200

    ajout du fichier pour la tache 4

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+Une classe CountedIterator
+"""
+
+
+class Fish:
+    """ class fish"""
+
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird:
+    """ class bird"""
+
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """ class Flyingfish qui hérite de fish et bird """
+
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swin(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
