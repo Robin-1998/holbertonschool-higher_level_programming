248eda23b868f15fa95fc7d25417838dde344c2d
Robin-1998 <10616@holbertonstudents.com>
2025-06-02 14:25:04 +0200

    ajout du fichier pour la tache 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+fonction qui lit un fichier texte et l'imprime sur stdout
+"""
+
+
 def read_file(filename=""):
-    f = open('filename', 'w', encoding="utf-8")
-    with open('filename', encoding="utf-8") as f:
-        read_file = f.read
+    """ fonction lecture de fichier """
+    with open('my_file_o.txt', encoding="utf-8") as f:
+        filename = f.read()
+
     f.closed
 True
