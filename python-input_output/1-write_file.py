125c7953939a08f6d47834a133393a2d504f2aa3
Robin-1998 <10616@holbertonstudents.com>
2025-06-03 09:10:20 +0200

    ajout du fichier pour la tache 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+fonction qui imprime une chaîne de caractère dans un fichier texte et
+retourne le nombre de caractère écrit
+"""
+
+
+def write_file(filename="", text=""):
+    """ imprime le nombre de caractère dans la chaîne de caractère """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return(f.write(text))
+
+# le 'w' est un mode d'ouverture du fichier, et il signifie write
+# permet d'écrire dans le fichier
