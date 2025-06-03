1f9e4e71e674ead920ec4f30d7ad5a8eed4bd489
Robin-1998 <10616@holbertonstudents.com>
2025-06-03 09:12:04 +0200

    correctif autopep8

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -10,7 +10,7 @@ retourne le nombre de caractère écrit
 def write_file(filename="", text=""):
     """ imprime le nombre de caractère dans la chaîne de caractère """
     with open(filename, 'w', encoding="utf-8") as f:
-        return(f.write(text))
+        return (f.write(text))
 
 # le 'w' est un mode d'ouverture du fichier, et il signifie write
 # permet d'écrire dans le fichier
