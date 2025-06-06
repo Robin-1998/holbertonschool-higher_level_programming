1a78822841410f24162f3167fe6723b20608fb33
Robin-1998 <10616@holbertonstudents.com>
2025-06-06 11:00:54 +0200

    ajout du fichier pour la tache 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+fonction qui renvoie la description du dictionnaire avec une strcuture
+de données simple (liste, dictionnaire, chaîne, entier et booléen) pour la
+sérialisation JSON d'un objet :
+"""
+
+
+def class_to_json(obj):
+    """ on retourne la description du dictionnaire """
+    return obj
