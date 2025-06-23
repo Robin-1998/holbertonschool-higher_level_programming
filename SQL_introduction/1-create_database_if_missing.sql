c6e75438d5b1b1a6ab9422104a5fa13d30205eeb
Robin-1998 <10616@holbertonstudents.com>
2025-06-23 11:24:09 +0200

    ajout du cas la database n'existe pas

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- Script that creates the databse hbtn_0c_0 in my MYSQL server
-CREATE DATABASE hbtn_0c_0;
+-- If the database already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;;
