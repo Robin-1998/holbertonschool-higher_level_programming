6e84097d6e34711b0fe85e5ffa86bb23d543ee82
Robin-1998 <10616@holbertonstudents.com>
2025-06-23 11:28:32 +0200

    syntaxe code

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Script that creates the databse hbtn_0c_0 in my MYSQL server
 -- If the database already exists, your script should not fail
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
