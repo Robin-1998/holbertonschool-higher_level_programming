3c055450809425423c72031914aa47c705eae31b
Robin-1998 <10616@holbertonstudents.com>
2025-06-23 15:16:07 +0200

    correctif tache 7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
--- Sript qui insert une nouvelle ligne dans la table first_place (base de donnée : hbtn_0c_0)
-INSERT INTO first_place (id, name)
-VALUES
+-- Sript qui insert une nouvelle ligne dans la table first_table (base de donnée : hbtn_0c_0)
+INSERT INTO first_table (id, name)
+VALUES 
 ('89', 'Best School');
