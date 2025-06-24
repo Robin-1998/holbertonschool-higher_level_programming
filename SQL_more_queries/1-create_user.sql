58726c752808301bcbfa39436d77ccc85c831bdb
Robin-1998 <10616@holbertonstudents.com>
2025-06-24 17:58:24 +0200

    correctif tache 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED by 'user_0d_1_pwd';
 -- on créé un utilisateur avec une conditon et on donne comme mot de passe : user_0d_1_pwd
 -- NOTE : il est obligatoire d'intégrer avec l'utilsateur l'hôte à partir duquel il se connecte (localhost)
-GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- On donne tous les priblèges de la base de donnée à l'utilisateur
 FLUSH PRIVILEGES;
 -- Les changements prennent effet immédiatement 
