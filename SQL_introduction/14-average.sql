589dd80b4a387ff67c0d2e2768c3e53cd8e5b142
Robin-1998 <10616@holbertonstudents.com>
2025-06-24 13:35:36 +0200

    ajout du fichier pour la tache 14

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Script qui calcule la moyenne des scores de tous les enregistrements de la table second_table de la base de données hbtn_0c_0
+SELECT AVG(score) AS average
+FROM second_table;
+-- la moyenne est calculé avec la fonction AVG(colonne)
