33f8b3813ed50d2145d1ae050ab4723e2e2099dd
Robin-1998 <10616@holbertonstudents.com>
2025-06-24 15:47:19 +0200

    ajout du fichier de la tache 0 + README

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- script qui répertorie tous les privilèges des utilisateurs MYSQL user_0d_1 et user_0d_2
+SHOW GRANTS FOR user_0d_1@localhost;
+-- on affiche les droits de chaque utilisateur
+SHOW GRANTS FOR user_0d_2@localhost;
