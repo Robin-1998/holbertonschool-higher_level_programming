33f9fa73dd5067780f3181dec7f0f09007d082b2
Robin-1998 <10616@holbertonstudents.com>
2025-06-24 18:48:29 +0200

    correctif tache 1 1.4

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- script qui créé l'utilisateur du serveur MYSQL user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED by 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- on créé un utilisateur avec une conditon et on donne comme mot de passe : user_0d_1_pwd
 -- NOTE : il est obligatoire d'intégrer avec l'utilsateur l'hôte à partir duquel il se connecte (localhost)
-GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 -- On donne tous les priblèges de la base de donnée à l'utilisateur
 FLUSH PRIVILEGES;
 -- Les changements prennent effet immédiatement 
