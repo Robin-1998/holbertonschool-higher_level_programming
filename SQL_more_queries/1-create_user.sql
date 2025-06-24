b2e3b65722e6dd8a0f8697b29ab129784ba918ae
Robin-1998 <10616@holbertonstudents.com>
2025-06-24 17:13:37 +0200

    ajout du fichier pour la tache 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- script qui créé l'utilisateur du serveur MYSQL user_0d_1
+CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED by 'user_0d_1_pwd';
+-- on créé un utilisateur avec une conditon et on donne comme mot de passe : user_0d_1_pwd
+GRANT ALL PRIVILEGES on *.* to user_0d_1 WITH GRANT OPTION;
+-- On donne tous les priblèges de la base de donnée à l'utilisateur
+FLUSH PRIVILEGES;
+-- Les changements prennent effet immédiatement 
