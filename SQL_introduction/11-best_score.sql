ba308cdd3468388490a965741245ca84cf11158a
Robin-1998 <10616@holbertonstudents.com>
2025-06-24 12:16:55 +0200

    ajout du fichier pour la tache 11

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Script qui répertorie tous les enregistrements avec un score >=10 dans la table second_table de la base de donnée hbtn_0c_0
+SELECT score, name
+FROM second_table
+WHERE score >=10
