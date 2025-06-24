95d4fafee5eff5a1502140230af20a4f298af6e8
Robin-1998 <10616@holbertonstudents.com>
2025-06-24 13:24:00 +0200

    correctif fichier erreur de signe

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,4 +1,4 @@
 -- Script qui supprime tous les enregistrements avec un score >=10 dans la table second_table de la base de donnée hbtn_0c_0
 DELETE FROM second_table
-WHERE score >= 5
+WHERE score <= 5
 
