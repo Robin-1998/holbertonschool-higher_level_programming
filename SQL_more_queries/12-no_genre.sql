a30f738d3f818df5eb9260e196a2c8466889bc2a
Robin-1998 <10616@holbertonstudents.com>
2025-06-25 16:52:49 +0200

    correctif syntax tache 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- Script qui liste toutes les émissions contenus dans hbtn_0d_tvshows sans genre lié
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 -- va rassembler tous les enregistrements de tv_shows et les enregistrements
 -- correspondant de tv_show_genres
 WHERE tv_show_genres.genre_id IS NULL
