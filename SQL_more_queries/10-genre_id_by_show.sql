c4eef9e477392e594b4ba0cbbdf514d35c03b213
Robin-1998 <10616@holbertonstudents.com>
2025-06-25 15:07:30 +0200

    ajout du fichier pour la tache 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- script qui liste toutes les émissions contenues dans hbtn_0d_tvshows qui ont au moins un genre lié
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
+
