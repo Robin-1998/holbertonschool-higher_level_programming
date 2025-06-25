5a0d5703e75cb244d6ffec67f90fa8721022b1cb
Robin-1998 <10616@holbertonstudents.com>
2025-06-25 14:02:29 +0200

    correctif tache 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Script qui liste toutes les villes contenu dans la base de donnée hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
