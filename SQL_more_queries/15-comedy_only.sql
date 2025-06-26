b6a4147944f2a5ee4c1648d8d297de0ec6bb8e92
Robin-1998 <10616@holbertonstudents.com>
2025-06-26 13:19:50 +0200

    oublie de majuscule à  AS

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- Script qui liste tous les films de comédie dans la database hbtn_0d_tvshows
 SELECT t.title
-FROM tv_shows as t
+FROM tv_shows AS t
 JOIN tv_show_genres AS sg ON t.id = sg.show_id
 JOIN tv_genres AS g ON g.id = sg.genre_id
 WHERE g.name = 'Comedy'
