583975bc6871fc12a5a7d4ea2af0bb29a80a4f39
Robin-1998 <10616@holbertonstudents.com>
2025-06-28 10:13:38 +0200

    test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
+"""
+Projet Holberton
+ROBIN D
+Script qui liste tout les états de la database hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
