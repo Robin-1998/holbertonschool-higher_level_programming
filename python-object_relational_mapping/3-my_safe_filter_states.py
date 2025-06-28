c485b6c3432a87d00ed5f853b8edf5cbbea87c2e
Robin-1998 <10616@holbertonstudents.com>
2025-06-28 18:52:54 +0200

    ajout du ficgier pour la tache 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Projet Holberton
+ROBIN D
+Script qui prend un argument et affiche toutes les valeurs de la table des
+états de hbtn_0e_0_usa dont le nom correspond à l'argument mais
+qui rajoute en plus une protection sécurité
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
+    cur = database.cursor()
+
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'"
+                .format(sys.argv[4]))
+    # Affichage  ligne par ligne
+    for state in cur.fetchall():
+        print(state)
