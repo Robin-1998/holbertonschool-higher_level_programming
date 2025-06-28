78ea9b7cdb1e7bc3271e4a5bae4a9fdc1106e1bb
Robin-1998 <10616@holbertonstudents.com>
2025-06-28 19:19:54 +0200

    ajout du fihier pour la tache 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Projet Holberton
+ROBIN D
+Script qui liste toutes les villes de la database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
+    cur = database.cursor()
+
+    commande = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC;
+    """
+    cur.execute(commande)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
