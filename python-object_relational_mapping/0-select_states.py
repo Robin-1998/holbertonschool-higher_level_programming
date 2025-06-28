d1ed115d387260b1c023cee7e5b893598cd69c81
Robin-1998 <10616@holbertonstudents.com>
2025-06-28 10:12:40 +0200

    suppression tout commentaire pour checker à la noix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,52 +1,10 @@
 #!/usr/bin/python3
-"""
-Projet Holberton
-ROBIN D
-Script qui liste tout les états de la database hbtn_0e_0_usa
-"""
-# Importation du module MySQLdb pour interagir avec une base MySQL
+# Lists all states from the database hbtn_0e_0_usa.
+import sys
 import MySQLdb
-from sys import argv
-# Importation de argv pour récupérer les arguments passés en 
-# ligne de commande
 
 if __name__ == "__main__":
-# Cette condition vérifie que le script est exécuté directement 
-# (pas importé dans un autre script)
-
-# Connexion à la base de données MySQL
-# Les paramètres de connexion sont récupérés dans argv :
-# argv[1] = nom d'utilisateur MySQL (ex: 'holberton')
-# argv[2] = mot de passe de l'utilisateur
-# argv[3] = nom de la base de données (ex: 'hbtn_0e_0_usa')
-    db = MySQLdb.connect(
-        host="localhost",
-# Adresse du serveur MySQL (ici la machine locale)
-        port=3306,
-# Port MySQL par défaut
-        user=argv[1],
-# Utilisateur MySQL passé en argument
-        passwd=argv[2],
-# Mot de passe passé en argument
-        db=argv[3]
-# Nom de la base de données passée en argument
-    )
-    
-    cur = db.cursor()
-# Création d'un curseur qui sert à exécuter des requêtes SQL
-
-# Exécution de la requête SQL qui sélectionne toutes les colonnes 
-# de la table 'states',
-# triées par l'id dans l'ordre croissant
-    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
-# cur.execute() renvoie le nombre de lignes retournées par la requête
-
-# On parcourt toutes les lignes obtenues, une par une, grâce au 
-# nombre de lignes
-    for i in range(num_rows):
-
-# cur.fetchone() récupère la ligne suivante du résultat sous forme de tuple,
-# ici (id, nom_de_l'état)
-
-        print(cur.fetchone())
-# Affiche la ligne récupérée
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
