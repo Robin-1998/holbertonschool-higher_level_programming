2b9558350fb112f5297bb3f1fca61c6d58cac64c
Robin-1998 <10616@holbertonstudents.com>
2025-06-28 13:03:56 +0200

    ajout du fichier pour la tache 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Projet Holberton
+ROBIN D
+Script qui prend un argument et affiche toutes les valeurs de la table des
+états de hbtn_0e_0_usa dont le nom correspond à l'argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
+    cur = database.cursor()
+
+    state_name = sys.argv[4]
+
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                .format(state_name))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
