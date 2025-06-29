a25b35c6a3f161e8c965fb6f77318444dd10db96
Robin-1998 <10616@holbertonstudents.com>
2025-06-29 16:50:50 +0200

    ajout du fichier pour la tache 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Projet Holberton
+ROBIN D
+Script qui liste tout les états contenant une lettra 'a'
+"""
+
+import sys
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_with_a = (
+        session.query(State)
+        .filter(State.name.like('%a%'))
+        .order_by(State.id)
+    )
+
+
+    # Affichage
+    for state in states_with_a:
+        print(f"{state.id}: {state.name}")
