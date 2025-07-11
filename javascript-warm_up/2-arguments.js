727441c65dff1ef1e2a45564e75dfa4ce7d48999
Robin-1998 <10616@holbertonstudents.com>
2025-07-11 12:56:33 +0200

    correctif des agrs

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
-#!/usr/bin/node
-if (process.argv.length <= 2) {
-  console.log('No argument');
-}
-if (process.argv.length === 1) {
-  console.log('Argument found')
-}
-else {
-  console.log('Arguments found')
+#!/usr/bin/env node
+if (process.argv.length === 2) {
+  console.log("No argument");
+} else if (process.argv.length === 3) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
 // process.argv est un tableau contenant les arguments
 // process.argv.length renvoie le nombre d'éléments utiliser
