3ce73add3913389590f57193f412d4d7878b5681
Robin-1998 <10616@holbertonstudents.com>
2025-07-11 11:27:28 +0200

    ajout du fichier pour la tache 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+if (process.argv.length <= 2) {
+  console.log('No argument');
+}
+if (process.argv.length === 1) {
+  console.log('Argument found')
+}
+else {
+  console.log('Arguments found')
+}
+// process.argv est un tableau contenant les arguments
+// process.argv.length renvoie le nombre d'éléments utiliser
+// === est l'équivalent de == dans une condition en langage C
