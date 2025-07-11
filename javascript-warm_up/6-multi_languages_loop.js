3cf9031721b45dd92a07ca35a0c6cc5d058eea53
Robin-1998 <10616@holbertonstudents.com>
2025-07-11 17:11:15 +0200

    ajout du fichier pour la tache 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const phrases = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (const phrase of phrases) {
+  console.log(phrase);
+}
+
+// le fait d'effectuer for ... if permet d'avoir la valeur
+// du tableau ou de la chaîne de caractère
+// et le for in permet d'itérer l'objet et d'indiquer le
+// nombre de clés et valeurs. vu qu'il y en a 3 dans ce cas il mettra 0,1,2
