03b6b4e73110da677be2b8352d932835cf216391
Robin-1998 <10616@holbertonstudents.com>
2025-07-14 10:50:04 +0200

    ajout du fichier pour la tache 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+document.querySelector('#red_header').addEventListener('click', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
+
+// la première ligne permet de cibler l'id red_header lorsque l'on clique dessus
